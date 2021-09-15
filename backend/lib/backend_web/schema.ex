1cb0de620a53b5cf717c61d09dc3c4d862f7bbff
Stamates <Stamates@users.noreply.github.com>
2021-09-15 09:05:47 -0400

    Setup Absinthe Schema with resolvers

--- backend/lib/backend_web/schema.ex
+++ backend/lib/backend_web/schema.ex
@@ -1,101 +1,63 @@
 defmodule BackendWeb.Schema do
+  @moduledoc """
+  GraphQL Schema for defining types, queries, and resolvers
+  """
   use Absinthe.Schema
 
-  object :todo do
+  alias Backend.Tasks
+
+  object :group do
     field :id, non_null(:id)
-    field :group, non_null(:string)
-    field :task, non_null(:string)
-    field :dependencies, non_null(list_of(:todo))
+    field :name, non_null(:string)
+    field :tasks, list_of(non_null(:task))
     field :completed_at, :string
   end
 
+  object :task do
+    field :id, non_null(:id)
+    field :group_id, non_null(:id)
+    field :name, non_null(:string)
+    field :dependencies, list_of(non_null(:task))
+    field :completed_at, :string
+    field :locked, non_null(:boolean)
+  end
+
   query do
-    @desc "Get all todos"
-    field :all_todos, non_null(list_of(non_null(:todo))) do
-      resolve(&all_tasks_resolver/3)
+    @desc "Get all groups"
+    field :groups, list_of(:group) do
+      resolve(fn _, _, _ -> {:ok, Tasks.list_groups() |> Tasks.preload_tasks()} end)
+    end
+
+    @desc "Get group by id"
+    field :group, non_null(:group) do
+      arg(:id, non_null(:id))
+      resolve(&fetch_group/3)
+    end
+
+    @desc "Get all tasks for group"
+    field :tasks, list_of(non_null(:task)) do
+      arg(:group_id, non_null(:id))
+      resolve(&all_tasks_for_group/3)
+    end
+
+    @desc "Get group by id"
+    field :task, non_null(:task) do
+      arg(:id, non_null(:id))
+      resolve(&fetch_task/3)
     end
   end
 
-  defp all_tasks_resolver(_, _, _) do
-    bank = %{
-      id: 1,
-      group: "Purchases",
-      task: "Go to the bank",
-      dependencies: [],
-      completed_at: nil
-    }
+  # defp all_groups(_root, _args, _info), do: {:ok, Tasks.list_groups()}
 
-    hammer = %{
-      id: 2,
-      group: "Purchases",
-      task: "Buy hammer",
-      dependencies: [
-        bank
-      ],
-      completed_at: nil
-    }
+  defp all_tasks_for_group(_root, %{group_id: group_id} = _args, _info) do
+    {:ok, group_id |> Tasks.list_group_tasks() |> Tasks.preload_dependencies()}
+  end
 
-    wood = %{
-      id: 3,
-      group: "Purchases",
-      task: "Buy wood",
-      dependencies: [
-        bank
-      ],
-      completed_at: nil
-    }
+  defp fetch_group(_root, %{id: id} = _args, _info) do
+    {:ok, id |> Tasks.get_group!() |> Tasks.preload_tasks()}
+  end
 
-    nails = %{
-      id: 4,
-      group: "Purchases",
-      task: "Buy nails",
-      dependencies: [
-        bank
-      ],
-      completed_at: nil
-    }
-
-    paint = %{
-      id: 5,
-      group: "Purchases",
-      task: "Buy paint",
-      dependencies: [
-        bank
-      ],
-      completed_at: nil
-    }
-
-    fasten = %{
-      id: 6,
-      group: "Build Airplane",
-      task: "Hammer nails into wood",
-      dependencies: [
-        hammer,
-        wood,
-        nails
-      ],
-      completed_at: nil
-    }
-
-    wings = %{
-      id: 7,
-      group: "Build Airplane",
-      task: "Paint wings",
-      dependencies: [
-        paint,
-        fasten
-      ],
-      completed_at: nil
-    }
-
-    snack = %{
-      id: 8,
-      group: "Build Airplane",
-      task: "Have a snack",
-      dependencies: [],
-      completed_at: nil
-    }
-
-    {:ok, [bank, hammer, wood, nails, paint, fasten, wings, snack]}
+  defp fetch_task(_root, %{id: id} = _args, _info) do
+    {:ok, id |> Tasks.get_task!() |> Tasks.preload_dependencies()}
   end
 end
---
+++ backend/test/backend_web/schema_test.exs
@@ -0,0 +1,192 @@
+defmodule BackendWeb.SchemaTest do
+  use BackendWeb.ConnCase
+
+  alias Backend.Tasks
+  alias Backend.Tasks.Task
+
+  @groups """
+  query groups {
+    groups {
+      id
+      name
+      tasks {
+        id
+      }
+      completed_at
+    }
+  }
+  """
+  test "query: groups", %{conn: conn} do
+    group = group_fixture()
+    task = task_fixture(group_id: group.id)
+    group_id = "#{group.id}"
+    name = group.name
+    task_id = "#{task.id}"
+    conn = post(conn, "/graphiql", %{"query" => @groups})
+
+    assert %{
+             "data" => %{
+               "groups" => [
+                 %{
+                   "id" => ^group_id,
+                   "name" => ^name,
+                   "completed_at" => nil,
+                   "tasks" => [
+                     %{"id" => ^task_id}
+                   ]
+                 }
+               ]
+             }
+           } = json_response(conn, 200)
+  end
+
+  @group """
+  query group($id: ID!) {
+    group(id: $id) {
+      id
+      name
+      tasks {
+        id
+      }
+      completed_at
+    }
+  }
+  """
+  test "query: group", %{conn: conn} do
+    group = group_fixture()
+    task = task_fixture(group_id: group.id)
+    group_id = "#{group.id}"
+    name = group.name
+    task_id = "#{task.id}"
+    conn = post(conn, "/graphiql", %{"query" => @group, "variables" => %{id: group_id}})
+
+    assert %{
+             "data" => %{
+               "group" => %{
+                 "id" => ^group_id,
+                 "name" => ^name,
+                 "completed_at" => nil,
+                 "tasks" => [
+                   %{"id" => ^task_id}
+                 ]
+               }
+             }
+           } = json_response(conn, 200)
+  end
+
+  @tasks_for_group """
+  query tasksForGroup($group_id: ID!) {
+    tasks(group_id: $group_id) {
+      id
+      name
+      group_id
+      dependencies {
+        id
+      }
+      completed_at
+      locked
+    }
+  }
+  """
+  test "query: all_tasks_for_group", %{conn: conn} do
+    group = group_fixture()
+    %Task{id: id, name: name} = task = task_fixture(group_id: group.id)
+
+    %Task{id: dependent_id, name: dependent_name} =
+      task_fixture(group_id: group.id, parent_id: task.id)
+
+    task_id = "#{id}"
+    dependent_task_id = "#{dependent_id}"
+    _other_group_task = task_fixture()
+
+    conn =
+      post(conn, "/graphiql", %{
+        "query" => @tasks_for_group,
+        "variables" => %{group_id: group.id}
+      })
+
+    assert %{
+             "data" => %{
+               "tasks" => [
+                 %{
+                   "id" => ^task_id,
+                   "name" => ^name,
+                   "completed_at" => nil,
+                   "locked" => true,
+                   "dependencies" => [
+                     %{
+                       "id" => ^dependent_task_id
+                     }
+                   ]
+                 },
+                 %{
+                   "id" => ^dependent_task_id,
+                   "name" => ^dependent_name,
+                   "completed_at" => nil,
+                   "locked" => false,
+                   "dependencies" => []
+                 }
+               ]
+             }
+           } = json_response(conn, 200)
+  end
+
+  @task """
+  query task($id: ID!) {
+    task(id: $id) {
+      id
+      name
+      group_id
+      dependencies {
+        id
+      }
+      completed_at
+      locked
+    }
+  }
+  """
+  test "query: task", %{conn: conn} do
+    group = group_fixture()
+    parent_task = task_fixture(group_id: group.id)
+    task = task_fixture(parent_id: parent_task.id)
+    group_id = "#{group.id}"
+    parent_task_id = "#{parent_task.id}"
+    name = parent_task.name
+    task_id = "#{task.id}"
+    conn = post(conn, "/graphiql", %{"query" => @task, "variables" => %{id: parent_task_id}})
+
+    assert %{
+             "data" => %{
+               "task" => %{
+                 "id" => ^parent_task_id,
+                 "group_id" => ^group_id,
+                 "name" => ^name,
+                 "completed_at" => nil,
+                 "locked" => true,
+                 "dependencies" => [
+                   %{"id" => ^task_id}
+                 ]
+               }
+             }
+           } = json_response(conn, 200)
+  end
+
+  # Private
+  defp group_fixture(attrs \\ %{}) do
+    {:ok, group} =
+      attrs
+      |> Enum.into(%{name: "some group"})
+      |> Tasks.create_group()
+
+    group
+  end
+
+  defp task_fixture(attrs \\ %{}) do
+    {:ok, task} =
+      attrs
+      |> Enum.into(%{name: "some task"})
+      |> Tasks.create_task()
+
+    task
+  end
+end
